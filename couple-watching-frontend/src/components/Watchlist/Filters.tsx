c13354e5897094b97b2371b750eb5c54d5146398
Filip Gałczk <filip.galczyk13@gmail.com>
2025-01-17 20:29:21 +0100

    Dodanie customowego switcha który posłuży za filtr w watchliście

---
+++ couple-watching-frontend/src/components/Watchlist/Filters.tsx
@@ -0,0 +1,77 @@
+import { Stack, Typography, Box, useTheme } from "@mui/material";
+import { motion } from "motion/react";
+import { Control, Controller, FieldValues, UseFormWatch } from "react-hook-form";
+import AdjustIcon from "@mui/icons-material/Adjust";
+
+type FiltersProps = {
+   control: Control<FieldValues, unknown>;
+   watch: UseFormWatch<FieldValues>;
+};
+
+const Filters = ({ control, watch }: FiltersProps) => {
+   const { palette } = useTheme();
+   return (
+      <Stack
+         spacing={2}
+         direction="row"
+         alignItems="center"
+         justifyContent="space-between"
+         sx={{ padding: 2, maxWidth: 375, backgroundColor: "background.paper", borderRadius: 4 }}
+         component={motion.div}
+         initial={{ opacity: 0 }}
+         animate={{ opacity: 1 }}
+      >
+         <Stack spacing={1}>
+            <Typography variant="body1">
+               <Typography component="span" display="inline" color="primary.main">
+                  The Nerd Mode
+               </Typography>{" "}
+               and{" "}
+               <Typography component="span" display="inline" color="secondary.main">
+                  The Cool Mode
+               </Typography>
+            </Typography>
+            <Typography variant="body2">Choose the way, you gonna see the list.</Typography>
+         </Stack>
+         <Controller
+            name="coolMode"
+            control={control}
+            render={({ field }) => (
+               <Box
+                  onClick={() => field.onChange(!watch("coolMode"))}
+                  sx={{
+                     padding: 1,
+                     backgroundColor: "#fff",
+                     borderRadius: 4,
+                     width: 44,
+                     height: 24,
+                     position: "relative",
+                     cursor: "pointer",
+                  }}
+               >
+                  <Box
+                     component={motion.div}
+                     whileHover={{ scale: 0.9 }}
+                     whileTap={{ scale: 0.45 }}
+                     animate={{
+                        x: watch("coolMode") ? 16 : 0,
+                        color: watch("coolMode") ? palette.secondary.main : palette.primary.main,
+                     }}
+                     sx={{
+                        position: "absolute",
+                        width: 20,
+                        height: 20,
+                        top: 0,
+                        left: 2,
+                        color: "primary.main",
+                     }}
+                     children={<AdjustIcon />}
+                  />
+               </Box>
+            )}
+         />
+      </Stack>
+   );
+};
+
+export default Filters;
--- couple-watching-frontend/src/pages/Watchlist/WatchlistPage.tsx
+++ couple-watching-frontend/src/pages/Watchlist/WatchlistPage.tsx
@@ -2,14 +2,18 @@ import { Card, Stack } from "@mui/material";
 import { WATCHLIST_STATIC_DATA } from "../../assets/WATCHLIST_STATIC_DATA";
 import PageTitle from "../../components/Layout/PageTitle";
 import WatchlistTable from "./WatchlistTable";
+import { useForm } from "react-hook-form";
+import Filters from "../../components/Watchlist/Filters";
 
 const WatchlistPage = () => {
+   const { control, watch } = useForm();
    return (
       <Stack spacing={4}>
          <PageTitle
             title="Watchlist"
             subtitle="This is a watchlist page, that will allow you to see what you wanna watch."
          />
+         <Filters control={control} watch={watch} />
          <Card variant="outlined" sx={{ borderRadius: 2, border: "2px solid", borderColor: "primary.main" }}>
             <WatchlistTable data={WATCHLIST_STATIC_DATA} />
          </Card>
