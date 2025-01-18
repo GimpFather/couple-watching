ebae090e51bada67bf5fc87317c600d090b51575
Filip Gałczk <filip.galczyk13@gmail.com>
2025-01-18 11:46:38 +0100

    Poprawki tekstowe

--- couple-watching-frontend/src/pages/Dashboard/DashboardPage.tsx
+++ couple-watching-frontend/src/pages/Dashboard/DashboardPage.tsx
@@ -1,7 +1,12 @@
 import PageTitle from "../../components/Layout/PageTitle";
 
 const DashboardPage = () => {
-   return <PageTitle title="Dashboard" subtitle="This is a dashboard page, that will allow you to see your stats." />;
+   return (
+      <PageTitle
+         title="Dashboard"
+         subtitle="🎥 Welcome to Your Watchlist Dashboard! 📊 Keep track of your stats and discover your next binge-worthy favorites!"
+      />
+   );
 };
 
 export default DashboardPage;
