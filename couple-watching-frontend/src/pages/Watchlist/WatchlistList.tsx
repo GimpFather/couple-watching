3f964e1d8ca3fd0b7ad17d6434bf5a9d8139a3a5
Filip Gałczyk <filip.galczyk@librus.pl>
2025-01-19 00:00:00 +0000

    Poprawki cool widoku watchlisty

--- couple-watching-frontend/src/pages/Watchlist/WatchlistList.tsx
+++ couple-watching-frontend/src/pages/Watchlist/WatchlistList.tsx
@@ -1,14 +1,16 @@
-import { Grid2 as Grid, Card, Stack, CardMedia, CardContent, Typography } from "@mui/material";
+import { Grid2 as Grid, Card, Stack, CardMedia, CardContent, Typography, Button, Box, useTheme } from "@mui/material";
 import { motion } from "motion/react";
 import { LazyLoadImage } from "react-lazy-load-image-component";
 import { Movie } from "../../types/Watchlist.types";
-import MovieActions from "./MovieActions";
+import AddCircleOutlineOutlinedIcon from "@mui/icons-material/AddCircleOutlineOutlined";
+import CancelOutlinedIcon from "@mui/icons-material/CancelOutlined";
 
 type WatchlistListProps = {
    data: Movie[];
 };
 
 const WatchlistList = ({ data }: WatchlistListProps) => {
+   const { palette } = useTheme();
    return (
       <Grid container spacing={2}>
          {data.map((movie) => (
@@ -22,57 +24,60 @@ const WatchlistList = ({ data }: WatchlistListProps) => {
                <Card
                   sx={{
                      height: 200,
-                     borderRadius: 2,
+                     borderRadius: 4,
+                     display: "flex",
                   }}
                >
-                  <Stack direction="row">
-                     <CardMedia>
-                        <LazyLoadImage
-                           src={movie.cover}
-                           alt={movie.title}
-                           width={150}
-                           effect="blur"
-                           style={{ borderLeft: "4px" }}
-                        />
-                     </CardMedia>
-                     <CardContent sx={{ padding: 2 }}>
-                        <Stack
-                           direction="row"
-                           alignItems="center"
-                           justifyContent="space-between"
-                           sx={{ paddingBottom: 3 }}
-                        >
-                           <Typography variant="h6">{movie.title}</Typography>
-                           <MovieActions />
-                        </Stack>
-                        <Stack spacing={1}>
+                  <CardMedia>
+                     <LazyLoadImage
+                        src={movie.cover}
+                        alt={movie.title}
+                        height={200}
+                        width={133}
+                        effect="blur"
+                        style={{ borderLeft: "4px", borderRight: `2px solid ${palette.primary.main}` }}
+                     />
+                  </CardMedia>
+                  <CardContent sx={{ padding: 2 }}>
+                     <Stack justifyContent="space-between" sx={{ height: "100%" }}>
+                        <Box>
+                           <Typography color="primary.main" variant="h6">
+                              {movie.title}
+                           </Typography>
                            <Stack direction="row" spacing={1}>
                               <Typography variant="body2">{`${movie.duration} minutes`}</Typography>
-                              <Typography variant="body2">{movie.productionYear}</Typography>
                               <Typography variant="body2">{movie.director}</Typography>
+                              <Typography variant="body2">{movie.productionYear}</Typography>
                            </Stack>
                            <Stack direction="row" spacing={1}>
                               {movie.genre.map((genre) => (
-                                 <Typography variant="body2">{genre}</Typography>
+                                 <Typography variant="body2">#{genre}</Typography>
                               ))}
                            </Stack>
-                           <Typography
-                              sx={{
-                                 display: "-webkit-box",
-                                 WebkitLineClamp: 2,
-                                 WebkitBoxOrient: "vertical",
-                                 overflow: "hidden",
-                                 textOverflow: "ellipsis",
-                                 lineHeight: 1.5,
-                                 maxHeight: "calc(1.5em * 2)",
-                                 whiteSpace: "normal",
-                              }}
+                        </Box>
+                        <Stack spacing={1} direction="row">
+                           <Button
+                              type="submit"
+                              variant="contained"
+                              startIcon={<AddCircleOutlineOutlinedIcon />}
+                              sx={{ borderRadius: 2, paddingX: 2 }}
                            >
-                              {movie.plot}
-                           </Typography>
+                              <Typography variant="body2" sx={{ textTransform: "initial" }}>
+                                 Mark as watched
+                              </Typography>
+                           </Button>
+                           <Button
+                              variant="text"
+                              startIcon={<CancelOutlinedIcon color="secondary" />}
+                              sx={{ borderRadius: 2, paddingX: 2 }}
+                           >
+                              <Typography color="secondary" variant="body2" sx={{ textTransform: "initial" }}>
+                                 Delete
+                              </Typography>
+                           </Button>
                         </Stack>
-                     </CardContent>
-                  </Stack>
+                     </Stack>
+                  </CardContent>
                </Card>
             </Grid>
          ))}
@@ -81,4 +86,3 @@ const WatchlistList = ({ data }: WatchlistListProps) => {
 };
 
 export default WatchlistList;
-
