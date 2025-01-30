1d5be2e4bff8912f0be21f354aa5a9ed7f3477aa
Filip Gałczk <filip.galczyk13@gmail.com>
2025-01-31 00:17:19 +0100

    Optymalizacja

--- couple-watching-frontend/src/components/General/InfoSection.tsx
+++ couple-watching-frontend/src/components/General/InfoSection.tsx
@@ -3,6 +3,8 @@ import { motion } from "motion/react";
 import { FormattedMessage } from "react-intl";
 import Button from "./Button";
 import { isString } from "lodash";
+import React from "react";
+import Loading from "./Loading";
 
 type InfoSectionProps = {
    title: React.ReactNode | string;
@@ -24,37 +26,45 @@ const InfoSection = ({ title, subtitle, emoji, primaryButton, secondaryButton }:
    const { breakpoints } = useTheme();
    const isMobile = useMediaQuery(breakpoints.down("sm"));
    return (
-      <Stack
-         component={motion.div}
-         initial={{ opacity: 0, y: -100 }}
-         animate={{ opacity: 1, y: 0 }}
-         spacing={4}
-         alignItems="center"
-         justifyContent="center"
-         sx={{ padding: 4, borderRadius: 4, backgroundColor: "background.paper", height: "100%" }}
-      >
-         <Stack spacing={1} alignItems="center" sx={{ textAlign: "center" }}>
-            {isString(emoji) ? <Typography variant="h2">{emoji}</Typography> : emoji}
-            <Typography variant="h4" fontWeight={700}>
-               {isString(title) ? <FormattedMessage id={title} /> : title}
-            </Typography>
-            <Typography variant="h6">{isString(subtitle) ? <FormattedMessage id={subtitle} /> : subtitle}</Typography>
-         </Stack>
-         {(primaryButton || secondaryButton) && (
-            <Stack direction={isMobile ? "column" : "row"} spacing={2}>
-               {primaryButton && (
-                  <Button variant="contained" startIcon={primaryButton.icon} onClick={() => primaryButton.action()}>
-                     <FormattedMessage id={primaryButton.caption} />
-                  </Button>
-               )}
-               {secondaryButton && (
-                  <Button variant="contained" startIcon={secondaryButton.icon} onClick={() => secondaryButton.action()}>
-                     <FormattedMessage id={secondaryButton.caption} />
-                  </Button>
-               )}
+      <React.Suspense fallback={<Loading isLoading={true} />}>
+         <Stack
+            component={motion.div}
+            initial={{ opacity: 0, y: -100 }}
+            animate={{ opacity: 1, y: 0 }}
+            spacing={4}
+            alignItems="center"
+            justifyContent="center"
+            sx={{ padding: 4, borderRadius: 4, backgroundColor: "background.paper", height: "100%" }}
+         >
+            <Stack spacing={1} alignItems="center" sx={{ textAlign: "center" }}>
+               {isString(emoji) ? <Typography variant="h2">{emoji}</Typography> : emoji}
+               <Typography variant="h4" fontWeight={700}>
+                  {isString(title) ? <FormattedMessage id={title} /> : title}
+               </Typography>
+               <Typography variant="h6">
+                  {isString(subtitle) ? <FormattedMessage id={subtitle} /> : subtitle}
+               </Typography>
             </Stack>
-         )}
-      </Stack>
+            {(primaryButton || secondaryButton) && (
+               <Stack direction={isMobile ? "column" : "row"} spacing={2}>
+                  {primaryButton && (
+                     <Button variant="contained" startIcon={primaryButton.icon} onClick={() => primaryButton.action()}>
+                        <FormattedMessage id={primaryButton.caption} />
+                     </Button>
+                  )}
+                  {secondaryButton && (
+                     <Button
+                        variant="contained"
+                        startIcon={secondaryButton.icon}
+                        onClick={() => secondaryButton.action()}
+                     >
+                        <FormattedMessage id={secondaryButton.caption} />
+                     </Button>
+                  )}
+               </Stack>
+            )}
+         </Stack>
+      </React.Suspense>
    );
 };
 
