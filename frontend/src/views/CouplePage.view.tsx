ffc696deabff6b87c42038734593c443df351da3
Filip Galczyk <filip.galczyk13@gmail.com>
2025-12-20 21:03:53 +0100

    CouplePage static

--- frontend/src/views/CouplePage.view.tsx
+++ frontend/src/views/CouplePage.view.tsx
@@ -3,10 +3,13 @@ import Stack from "@mui/material/Stack";
 import Typography from "@mui/material/Typography";
 import NBButton from "~/components/NeoBrutalism/NBButton";
 import showToast from "~/components/Toasts/showToast";
-import { SmileyXEyesIcon } from "@phosphor-icons/react";
+import { SealCheckIcon } from "@phosphor-icons/react";
 import PhoneContainer from "~/components/Layout/PhoneContainer";
 
 const CouplePage = () => {
+   const isPaired = false;
+   const isCustomized = false;
+
    const handleCoupleButton = () => {
       showToast({
          title: "Show me the couple!",
@@ -20,16 +23,37 @@ const CouplePage = () => {
       <PhoneContainer>
          <Stack direction="column" gap={2}>
             <Typography variant="headingExtraLarge">Couple</Typography>
-            <Typography variant="bodyExtraLarge">This is the couple! 💑</Typography>
+            <Typography variant="bodyExtraLarge">It's time to set up your couple! 💑</Typography>
             <Divider />
+            <Typography variant="bodyMedium">Start with customizing your account details . . .</Typography>
+            <NBButton disabled={isCustomized} onClick={() => handleCoupleButton()}>
+               Customize my account details
+            </NBButton>
+            <Typography variant="bodyMedium">. . . then, set up your couple details.</Typography>
+            <NBButton disabled={!isCustomized} onClick={() => handleCoupleButton()}>
+               Make a pair
+            </NBButton>
+            <Divider />
+            <Typography variant="bodyMedium">
+               If everything is set up correctly, buttons below will be enabled, and you can start toodling around the
+               app or delete your pair. Don't worry, you can always set up your couple again. This is purely for testing
+               purposes.
+            </Typography>
             <Stack direction="row" gap={2}>
-               <NBButton icon={<SmileyXEyesIcon />} onClick={() => handleCoupleButton()}>
-                  Show me the couple!
+               <NBButton
+                  color="success"
+                  disabled={!isPaired}
+                  onClick={() => handleCoupleButton()}
+                  icon={<SealCheckIcon />}
+               >
+                  Check
+               </NBButton>
+               <NBButton color="danger" disabled={!isPaired} onClick={() => handleCoupleButton()}>
+                  Delete pair
                </NBButton>
             </Stack>
          </Stack>
       </PhoneContainer>
    );
 };
-
 export default CouplePage;
