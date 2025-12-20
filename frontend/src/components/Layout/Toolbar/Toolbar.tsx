d58d2b6202299f11a827292963f4fca5a062048a
Filip Galczyk <filip.galczyk13@gmail.com>
2025-12-20 20:40:02 +0100

    Mobile fix

--- frontend/src/components/Layout/Toolbar/Toolbar.tsx
+++ frontend/src/components/Layout/Toolbar/Toolbar.tsx
@@ -63,7 +63,6 @@ const Toolbar = () => {
             right: 0,
             borderTop: `1px solid ${palette.accent.main}`,
             padding: "6px 8px",
-            marginBottom: "28px",
          }}
       >
          <ToolbarItem
