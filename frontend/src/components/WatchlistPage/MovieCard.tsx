71c2ee722a4a632b1ff35f2bf04d2b0af5b1fc78
Filip Galczyk <filip.galczyk13@gmail.com>
2026-01-23 05:54:45 +0100

    Początki prac nad widokiem watchlisty

---
+++ frontend/src/components/WatchlistPage/MovieCard.tsx
@@ -0,0 +1,40 @@
+import { Typography } from "@mui/material";
+import { Stack } from "@mui/material";
+import NBCard from "../NeoBrutalism/NBCard";
+import type { MovieSearch } from "~/api/types/omdb";
+import NBButton from "../NeoBrutalism/NBButton";
+import NBIconButton from "../NeoBrutalism/NBIconButton";
+import { DeviceRotateIcon } from "@phosphor-icons/react";
+
+const MovieCard = ({ movie }: { movie: MovieSearch }) => {
+    const optimizedPoster = movie.poster.replace("_SX300.", "_SX600.");
+
+    return (
+        <NBCard sx={{ padding: '2.5px', height: 500, width: 344 }}>
+            <Stack direction="column" justifyContent='flex-end' gap={1} sx={{
+                padding: '12px',
+                backgroundImage: `linear-gradient(180deg, rgba(12, 12, 12, 0) 0%, #0C0C0C 95%, #0C0C0C 100%), url(${optimizedPoster})`,
+                backgroundSize: "cover",
+                backgroundPosition: "center",
+                backgroundRepeat: "no-repeat",
+                height: "100%",
+                width: "100%",
+                borderRadius: "12px",
+            }}>
+                <Stack justifyContent="space-between" gap={1.5}>
+                    <Typography variant="headingLarge" color="common.white">
+                        {movie.title}
+                    </Typography>
+                    <Stack direction="row" gap={1}>
+                        <NBButton fullWidth>
+                            Add to watchlist
+                        </NBButton>
+                        <NBIconButton color="accent" icon={<DeviceRotateIcon />} onClick={() => { }} />
+                    </Stack>
+                </Stack>
+            </Stack>
+        </NBCard>
+    );
+};
+
+export default MovieCard;
--- frontend/src/views/WatchlistPage.view.tsx
+++ frontend/src/views/WatchlistPage.view.tsx
@@ -4,22 +4,27 @@ import { PlusCircleIcon } from "@phosphor-icons/react";
 import { useState } from "react";
 import { useGetOMDbSearchForMovies } from "~/api/hooks/omdb";
 import PhoneContainer from "~/components/Layout/PhoneContainer";
-import NBCard from "~/components/NeoBrutalism/NBCard";
 import NBIconButton from "~/components/NeoBrutalism/NBIconButton";
 import NBSearchBox from "~/components/NeoBrutalism/NBSearchBox";
 import NBChip from "~/components/NeoBrutalism/NBChip";
+import { playSound } from "~/hooks/useSound";
+import MovieCard from "~/components/WatchlistPage/MovieCard";
+import { motion } from "motion/react";
+import { AnimatePresence } from "motion/react";
 
-const LibraryPage = () => {
-   const [searchTerm, setSearchTerm] = useState("");
-   const { data: omdbSearchData } = useGetOMDbSearchForMovies({ title: searchTerm });
+const WatchlistPage = () => {
+   const [searchTerm, setSearchTerm] = useState("Edgerunners");
+   const { data: omdbSearchData } = useGetOMDbSearchForMovies({ title: searchTerm, });
 
    const [selectedChip, setSelectedChip] = useState(['All']);
 
    const handleChipClick = (chip: string) => {
       if (isChipSelected(chip)) {
          setSelectedChip(selectedChip.filter((c) => c !== chip));
+         playSound("BUTTON_CLICK_START");
       } else {
          setSelectedChip([...selectedChip, chip]);
+         playSound("BUTTON_CLICK_END");
       }
    };
 
@@ -29,7 +34,7 @@ const LibraryPage = () => {
 
    return (
       <PhoneContainer>
-         <Stack direction="column" gap={2}>
+         <Stack direction="column" gap={2} sx={{ paddingBottom: "20px" }}>
             <Stack direction="row" justifyContent="space-between" alignItems="center">
                <Typography variant="headingExtraLarge">Watchlist</Typography>
                <NBIconButton icon={<PlusCircleIcon />} />
@@ -40,22 +45,26 @@ const LibraryPage = () => {
                <NBChip label="Movies" onClick={() => handleChipClick("Movies")} active={selectedChip.includes("Movies")} />
                <NBChip label="Series" onClick={() => handleChipClick("Series")} active={selectedChip.includes("Series")} />
             </Stack>
-            <Stack direction="column" gap={2}>
-               {omdbSearchData?.map((movie) => (
-                  <NBCard key={movie.imdbID}>
-                     <Stack direction="column" gap={1} sx={{ padding: 2 }}>
-                        <Stack direction="row" justifyContent="space-between" alignItems="center">
-                           <Typography variant="emphasizedBodyLarge">{movie.title}</Typography>
-                           <Typography variant="bodySmall">{movie.year}</Typography>
-                        </Stack>
-                        <img src={movie.poster} alt={movie.title} style={{ width: "100%", height: "100%", objectFit: "cover", borderRadius: "12px", marginTop: 4 }} />
-                     </Stack>
-                  </NBCard>
+         </Stack>
+         <Stack direction="column" gap={2}>
+            <AnimatePresence mode="wait" initial={true}>
+               {omdbSearchData?.map((movie, index) => (
+                  <>
+                     {movie && (
+                        <motion.div
+                           initial={{ opacity: 0, }}
+                           animate={{ opacity: 1, }}
+                           exit={{ opacity: 0, }}
+                           transition={{ duration: 0.3, ease: [0.87, 0, 0.13, 1], delay: index * 0.1 }}
+                        >
+                           <MovieCard key={movie.imdbID} movie={movie} />
+                        </motion.div>
+                     )}</>
                ))}
-            </Stack>
+            </AnimatePresence>
          </Stack>
       </PhoneContainer>
    );
 };
 
-export default LibraryPage;
+export default WatchlistPage;
