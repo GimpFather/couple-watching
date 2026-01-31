efcdd2bbd36ad94d3fe1e2b4313249f49af41035
Filip Galczyk <filip.galczyk13@gmail.com>
2026-01-31 21:42:35 +0100

    Usunięcie zbędnych pozostałości po datepickerach

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -21,9 +21,6 @@ import AuthProvider from "~/context/auth/AuthContext";
 import AppRouting from "~/router/AppRouting";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-import { LocalizationProvider } from '@mui/x-date-pickers';
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
-
 initSounds(Object.values(SOUNDS));
 
 const queryClient = new QueryClient();
@@ -33,15 +30,13 @@ createRoot(document.getElementById("root")!).render(
       <BrowserRouter>
          <ThemeProvider theme={getTheme("light", palette)}>
             <QueryClientProvider client={queryClient}>
-               <LocalizationProvider dateAdapter={AdapterDayjs}>
-                  <AuthProvider>
-                     <CssBaseline />
-                     <Toaster position="top-center" />
-                     <AppRouting />
-                  </AuthProvider>
-               </LocalizationProvider>
+               <AuthProvider>
+                  <CssBaseline />
+                  <Toaster position="top-center" />
+                  <AppRouting />
+               </AuthProvider>
             </QueryClientProvider>
          </ThemeProvider>
       </BrowserRouter>
-   </StrictMode>
+   </StrictMode >
 );
